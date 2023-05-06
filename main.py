fbcdb4b52a625b2d9844d5e4e1dceb2e9cbc1ab8
cls1845437080 <113126055+cls1845437080@users.noreply.github.com>
2023-05-06 13:10:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ if __name__ == "__main__":
     if note_ch == "" and note_en == "":
         # 获取词霸每日金句
         note_ch, note_en = get_ciba()
-#     # 公众号推送消息
-#     for user in users:
-#         send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
-#     os.system("pause")
+    # 公众号推送消息
+    for user in users:
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+    os.system("pause")
